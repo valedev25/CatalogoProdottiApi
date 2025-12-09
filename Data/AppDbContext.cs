a3f0765c8552acc6906dd6fb81002b375df40e98
V Tuzzi <Valentina.Tuzzi@eng.it>
2025-12-09 12:00:08 +0100

    Add DbContext ed EntityFramework Core

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using CatalogoProdottiApi.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace CatalogoProdottiApi.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Prodotto> Prodotti => Set<Prodotto>();
+
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options) { }
+    }
+}
