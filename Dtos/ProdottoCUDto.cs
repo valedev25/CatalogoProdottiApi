9024d8bbab59675bea20ed90118a77b083db41e7
V Tuzzi <Valentina.Tuzzi@eng.it>
2025-12-09 11:54:34 +0100

    Add Dtos

---
+++ Dtos/ProdottoCUDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CatalogoProdottiApi.Dtos
+{
+    public class ProdottoCUDto
+    {        
+        public string Nome { get; set; } = string.Empty;
+
+        public decimal Prezzo { get; set; }
+      
+        public string Categoria { get; set; } = string.Empty;
+    }
+}
---
+++ Dtos/ProdottoResponseDto.cs
@@ -0,0 +1,10 @@
+﻿namespace CatalogoProdottiApi.Dtos
+{
+    public class ProdottoResponseDto
+    {
+        public Guid Id { get; set; }
+        public string Nome { get; set; } = string.Empty;
+        public decimal Prezzo { get; set; }
+        public string Categoria { get; set; } = string.Empty;
+    }
+}
