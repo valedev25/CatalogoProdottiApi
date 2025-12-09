b43762fb85e93b4ec937c701c07e07099ae20586
V Tuzzi <Valentina.Tuzzi@eng.it>
2025-12-09 12:03:13 +0100

    Add ExceptionMiddleware ed integrazione in program.cs

---
+++ Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,40 @@
+﻿using CatalogoProdottiApi.Exceptions;
+using System.Text.Json;
+
+namespace CatalogoProdottiApi.Middleware
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ExceptionMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (ProdottoExceptions ex)
+            {
+                context.Response.ContentType = "application/json";
+                context.Response.StatusCode = ex.Type switch
+                {
+                    ProdottoExceptions.ErrorType.NotFound => StatusCodes.Status404NotFound,
+                    ProdottoExceptions.ErrorType.Invalid => StatusCodes.Status400BadRequest,
+                    _ => StatusCodes.Status500InternalServerError
+                };
+
+                await context.Response.WriteAsJsonAsync(new { errore = ex.Message });
+            }
+            catch (Exception ex)
+            {
+                context.Response.StatusCode = 500;
+                await context.Response.WriteAsJsonAsync(new { errore = ex.Message });
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using CatalogoProdottiApi.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -8,6 +10,9 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+// Middleware
+app.UseMiddleware<ExceptionMiddleware>();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
